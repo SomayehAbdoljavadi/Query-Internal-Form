79bbb78f29fc92e1ef9f58182345972bae32ac29
Somayeh Abdoljavadi <somabd01@conmore.se>
2023-10-02 11:07:02 +0200

    Add QueryInternalForm Project

---
+++ Backup/frmMain.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace QueryInternalForm
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Query = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.txt_PF = new System.Windows.Forms.TextBox();
+            this.lbl_PF = new System.Windows.Forms.Label();
+            this.lbl_IF = new System.Windows.Forms.Label();
+            this.txt_IF = new System.Windows.Forms.TextBox();
+            this.lbl_Query = new System.Windows.Forms.Label();
+            this.btn_Q1 = new System.Windows.Forms.Button();
+            this.btn_Q3 = new System.Windows.Forms.Button();
+            this.btn_Q2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_Query
+            // 
+            this.txt_Query.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_Query.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Query.Location = new System.Drawing.Point(16, 60);
+            this.txt_Query.Margin = new System.Windows.Forms.Padding(7);
+            this.txt_Query.Multiline = true;
+            this.txt_Query.Name = "txt_Query";
+            this.txt_Query.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_Query.Size = new System.Drawing.Size(400, 400);
+            this.txt_Query.TabIndex = 1;
+            this.txt_Query.Text = "Select\r\n\r\nFrom\r\n\r\nWhere\r\n\r\nGroup By\r\n\r\nHaving\r\n\r\nOrder By\r\n";
+            this.txt_Query.WordWrap = false;
+            // 
+            // btn
+            // 
+            this.btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn.Location = new System.Drawing.Point(680, 474);
+            this.btn.Margin = new System.Windows.Forms.Padding(7);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(150, 30);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "! Click Me !";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_PF
+            // 
+            this.txt_PF.BackColor = System.Drawing.Color.White;
+            this.txt_PF.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_PF.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_PF.Location = new System.Drawing.Point(16, 518);
+            this.txt_PF.Margin = new System.Windows.Forms.Padding(7);
+            this.txt_PF.Multiline = true;
+            this.txt_PF.Name = "txt_PF";
+            this.txt_PF.ReadOnly = true;
+            this.txt_PF.Size = new System.Drawing.Size(814, 25);
+            this.txt_PF.TabIndex = 2;
+            this.txt_PF.TabStop = false;
+            this.txt_PF.WordWrap = false;
+            // 
+            // lbl_PF
+            // 
+            this.lbl_PF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_PF.Location = new System.Drawing.Point(16, 474);
+            this.lbl_PF.Margin = new System.Windows.Forms.Padding(7);
+            this.lbl_PF.Name = "lbl_PF";
+            this.lbl_PF.Size = new System.Drawing.Size(150, 30);
+            this.lbl_PF.TabIndex = 3;
+            this.lbl_PF.Text = "Parenthesis Form";
+            this.lbl_PF.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_IF
+            // 
+            this.lbl_IF.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_IF.Location = new System.Drawing.Point(430, 16);
+            this.lbl_IF.Margin = new System.Windows.Forms.Padding(7);
+            this.lbl_IF.Name = "lbl_IF";
+            this.lbl_IF.Size = new System.Drawing.Size(400, 30);
+            this.lbl_IF.TabIndex = 4;
+            this.lbl_IF.Text = "Internal Form";
+            this.lbl_IF.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_IF
+            // 
+            this.txt_IF.BackColor = System.Drawing.Color.White;
+            this.txt_IF.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txt_IF.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_IF.Location = new System.Drawing.Point(430, 60);
+            this.txt_IF.Margin = new System.Windows.Forms.Padding(7);
+            this.txt_IF.Multiline = true;
+            this.txt_IF.Name = "txt_IF";
+            this.txt_IF.ReadOnly = true;
+            this.txt_IF.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_IF.Size = new System.Drawing.Size(400, 400);
+            this.txt_IF.TabIndex = 5;
+            this.txt_IF.TabStop = false;
+            this.txt_IF.WordWrap = false;
+            // 
+            // lbl_Query
+            // 
+            this.lbl_Query.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Query.Location = new System.Drawing.Point(16, 16);
+            this.lbl_Query.Margin = new System.Windows.Forms.Padding(7);
+            this.lbl_Query.Name = "lbl_Query";
+            this.lbl_Query.Size = new System.Drawing.Size(400, 30);
+            this.lbl_Query.TabIndex = 6;
+            this.lbl_Query.Text = "Query";
+            this.lbl_Query.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_Q1
+            // 
+            this.btn_Q1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Q1.Location = new System.Drawing.Point(19, 19);
+            this.btn_Q1.Margin = new System.Windows.Forms.Padding(10, 10, 2, 10);
+            this.btn_Q1.Name = "btn_Q1";
+            this.btn_Q1.Size = new System.Drawing.Size(27, 24);
+            this.btn_Q1.TabIndex = 7;
+            this.btn_Q1.TabStop = false;
+            this.btn_Q1.Text = "1";
+            this.btn_Q1.UseVisualStyleBackColor = true;
+            this.btn_Q1.Click += new System.EventHandler(this.btn_Q1_Click);
+            // 
+            // btn_Q3
+            // 
+            this.btn_Q3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Q3.Location = new System.Drawing.Point(81, 19);
+            this.btn_Q3.Margin = new System.Windows.Forms.Padding(2, 10, 10, 10);
+            this.btn_Q3.Name = "btn_Q3";
+            this.btn_Q3.Size = new System.Drawing.Size(27, 24);
+            this.btn_Q3.TabIndex = 8;
+            this.btn_Q3.TabStop = false;
+            this.btn_Q3.Text = "3";
+            this.btn_Q3.UseVisualStyleBackColor = true;
+            this.btn_Q3.Click += new System.EventHandler(this.btn_Q3_Click);
+            // 
+            // btn_Q2
+            // 
+            this.btn_Q2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Q2.Location = new System.Drawing.Point(50, 19);
+            this.btn_Q2.Margin = new System.Windows.Forms.Padding(2, 10, 2, 10);
+            this.btn_Q2.Name = "btn_Q2";
+            this.btn_Q2.Size = new System.Drawing.Size(27, 24);
+            this.btn_Q2.TabIndex = 9;
+            this.btn_Q2.TabStop = false;
+            this.btn_Q2.Text = "2";
+            this.btn_Q2.UseVisualStyleBackColor = true;
+            this.btn_Q2.Click += new System.EventHandler(this.btn_Q2_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 559);
+            this.Controls.Add(this.btn_Q2);
+            this.Controls.Add(this.btn_Q3);
+            this.Controls.Add(this.btn_Q1);
+            this.Controls.Add(this.lbl_Query);
+            this.Controls.Add(this.txt_IF);
+            this.Controls.Add(this.lbl_IF);
+            this.Controls.Add(this.lbl_PF);
+            this.Controls.Add(this.txt_PF);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.txt_Query);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Query Internal Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Query;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.TextBox txt_PF;
+        private System.Windows.Forms.Label lbl_PF;
+        private System.Windows.Forms.Label lbl_IF;
+        private System.Windows.Forms.TextBox txt_IF;
+        private System.Windows.Forms.Label lbl_Query;
+        private System.Windows.Forms.Button btn_Q1;
+        private System.Windows.Forms.Button btn_Q3;
+        private System.Windows.Forms.Button btn_Q2;
+    }
+}
+
---
+++ Backup/frmMain.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace QueryInternalForm {
+    public partial class frmMain : Form {
+        public frmMain() {
+            InitializeComponent();
+        }
+
+        List<string> zFrom;
+
+        private void button1_Click(object sender, EventArgs e) {
+            
+            List<string> zSelect = new List<string>();
+            List<string> zSelectMathFunctions = new List<string>();
+            zFrom = new List<string>();
+            List<string> zWhere = new List<string>();
+            List<string> zJoin = new List<string>();
+            List<string> zWhereMathFunctions = new List<string>();
+            List<string> zGroupBy = new List<string>();
+            List<string> zHaving = new List<string>();
+            List<string> zHavingMathFunctions = new List<string>();
+            List<string> zOrderBy = new List<string>();
+
+            bool Select, From, Where, GroupBy, Having, OrderBy;
+            Select = From = Where = GroupBy = Having = OrderBy = false;
+            string[] QueryLines = txt_Query.Text.Split(zEnter().ToCharArray());
+            string Line = "";
+
+            for (int c = 0; c < QueryLines.Length; c++) {
+                Line = QueryLines[c].Trim().ToLower();
+                if (Line != "") {
+                    if (Line.Substring(Line.Length - 1, 1) == ",") {
+                        Line = Line.Substring(0, Line.Length - 1);
+                    }
+                    if (Select == false) {
+                        if (c == 0 && Line != "select") {
+                            MessageBox.Show(this, "This is not a ''Select Query''", "!!! ERROR !!!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                            return;
+                        } else {
+                            Select = true;
+                        }
+                    } else {
+                        if (From == false) {
+                            if (Line != "from") {
+                                if (IsMath(Line) == true) {
+                                    zSelectMathFunctions.Add(Line);
+                                } else {
+                                    zSelect.Add(Line);
+                                }
+                            } else {
+                                if (zSelect.Count == 0) {
+                                    MessageBox.Show(this, "This is not a ''Select Query''", "!!! ERROR !!!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                                    return;
+                                }
+                                From = true;
+                            }
+                        } else {
+                            if (Where == false) {
+                                if (Line != "where") {
+                                    zFrom.Add(Line);
+                                } else {
+                                    Where = true;
+                                }
+                            } else {
+                                if (GroupBy == false) {
+                                    if (Line != "group by") {
+                                        if (Line != "and" && Line != "or") {
+                                            if (IsJoin(Line) == true) {
+                                                zJoin.Add(Line);
+                                            } else {
+                                                if (IsMath(Line) == true) {
+                                                    zWhereMathFunctions.Add(Line);
+                                                }
+                                                zWhere.Add(Line);
+                                            }
+                                        }
+                                    } else {
+                                        GroupBy = true;
+                                    }
+                                } else {
+                                    if (Having == false) {
+                                        if (Line != "having") {
+                                            zGroupBy.Add(Line);
+                                        } else {
+                                            if (zGroupBy.Count > 0) {
+                                                bool Err = false;
+                                                if (zSelect.Count != zGroupBy.Count) {
+                                                    Err = true;
+                                                }
+                                                for (int i = 0; i < zGroupBy.Count; i++) {
+                                                    if (zSelect[i] != zGroupBy[i]) {
+                                                        Err = true;
+                                                    }
+                                                }
+                                                if (Err == true) {
+                                                    MessageBox.Show(this, "This is not a ''Select Query''", "!!! ERROR !!!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                                                    return;
+                                                }
+                                            }
+                                            Having = true;
+                                        }
+                                    } else {
+                                        if (OrderBy == false) {
+                                            if (Line != "order by") {
+                                                if (zGroupBy.Count == 0) {
+                                                    MessageBox.Show(this, "This is not a ''Select Query''", "!!! ERROR !!!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                                                    return;
+                                                }
+                                                if (IsMath(Line) == true) {
+                                                    zHavingMathFunctions.Add(Line);
+                                                }
+                                                zHaving.Add(Line);
+                                            } else {
+                                                OrderBy = true;
+                                            }
+                                        } else {
+                                            zOrderBy.Add(Line);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Select && From && Where && GroupBy && Having && OrderBy) {
+
+
+
+                // >>>>>>>>>>>>> ================ <<<<<<<<<<<<< \\
+                // >>>>>>>>>>>>> Parenthesis Form <<<<<<<<<<<<< \\
+                // >>>>>>>>>>>>> ================ <<<<<<<<<<<<< \\
+
+                string PForm = "";
+                // Join
+                if (zWhere.Count > 0) {
+                    PForm += "( ";
+                }
+                PForm += "JOIN ( ";
+                for (int c = 0; c < zJoin.Count; c++) {
+                    string[] Join = zJoin[c].Split('=');
+                    PForm += Join[0].Split('.')[0].Trim() + " WITH " + Join[1].Split('.')[0].Trim() + " ON " + Join[0].Split('.')[1].Trim() + " , ";
+                }
+                PForm = PForm.Substring(0, PForm.Length - 3) + " ) ";
+                // Where
+                if (zWhere.Count > 0) {
+                    PForm += "WHERE ( ";
+                    for (int c = 0; c < zWhere.Count; c++) {
+                        PForm += zWhere[c] + " , ";
+                    }
+                    PForm = PForm.Substring(0, PForm.Length - 3) + " ) ) ";
+                }
+                // Group By
+                if (zHaving.Count > 0) {
+                    PForm += "( ";
+                }
+                if (zGroupBy.Count > 0) {
+                    PForm += "GROUP BY ( ";
+                    for (int c = 0; c < zGroupBy.Count; c++) {
+                        PForm += zGroupBy[c] + " , ";
+                    }
+                    PForm = PForm.Substring(0, PForm.Length - 3) + " ) ";
+                }
+                // Having
+                if (zHaving.Count > 0) {
+                    PForm += "WHERE ( ";
+                    for (int c = 0; c < zHaving.Count; c++) {
+                        PForm += zHaving[c] + " , ";
+                    }
+                    PForm = PForm.Substring(0, PForm.Length - 3) + " ) ) ";
+                }
+                // Project
+                PForm += "{ ";
+                for (int c = 0; c < zSelect.Count; c++) {
+                    PForm += zSelect[c] + " , ";
+                }
+                for (int c = 0; c < zSelectMathFunctions.Count; c++) {
+                    PForm += zSelectMathFunctions[c] + " , ";
+                }
+                PForm = PForm.Substring(0, PForm.Length - 3) + " }";
+                // Order By
+                if (zOrderBy.Count > 0) {
+                    PForm += " ORDER BY ( ";
+                    for (int c = 0; c < zOrderBy.Count; c++) {
+                        PForm += zOrderBy[c] + " , ";
+                    }
+                    PForm = PForm.Substring(0, PForm.Length - 3) + " )";
+                }
+                // [TextBox]
+                txt_PF.Text = PForm;
+
+
+
+                // >>>>>>>>>>>>> ============= <<<<<<<<<<<<< \\
+                // >>>>>>>>>>>>> Internal Form <<<<<<<<<<<<< \\
+                // >>>>>>>>>>>>> ============= <<<<<<<<<<<<< \\
+
+                int IFormStepsCounter = 0;
+                string IForm = "";
+                // Where Math Function
+                if (zWhereMathFunctions.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Math Function (Select Where) ]" + zEnter();
+                    for (int c = 0; c < zWhereMathFunctions.Count; c++) {
+                        IForm += zWhereMathFunctions[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // Where
+                if (zWhere.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Select Where ]" + zEnter();
+                    for (int c = 0; c < zWhere.Count; c++) {
+                        IForm += zWhere[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // Join
+                IForm += "[ " + ++IFormStepsCounter + " : Join ]" + zEnter();
+                for (int c = 0; c < zJoin.Count; c++) {
+                    string[] Join = zJoin[c].Split('=');
+                    IForm += Join[0].Split('.')[0].Trim() + " , " + Join[1].Split('.')[0].Trim() + " => " + Join[0].Split('.')[1].Trim() + zEnter();
+                }
+                IForm += zEnter();
+                // Having Math Function
+                if (zHavingMathFunctions.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Math Function (Having) ]" + zEnter();
+                    for (int c = 0; c < zHavingMathFunctions.Count; c++) {
+                        IForm += zHavingMathFunctions[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // Having
+                if (zHaving.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Having ]" + zEnter();
+                    for (int c = 0; c < zHaving.Count; c++) {
+                        IForm += zHaving[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // Group By
+                if (zGroupBy.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Group By ]" + zEnter();
+                    for (int c = 0; c < zGroupBy.Count; c++) {
+                        IForm += zGroupBy[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // Project Math Function
+                if (zSelectMathFunctions.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Math Function (Project) ]" + zEnter();
+                    for (int c = 0; c < zSelectMathFunctions.Count; c++) {
+                        IForm += zSelectMathFunctions[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // Project
+                IForm += "[ " + ++IFormStepsCounter + " : Project ]" + zEnter();
+                for (int c = 0; c < zSelect.Count; c++) {
+                    IForm += zSelect[c] + zEnter();
+                }
+                for (int c = 0; c < zSelectMathFunctions.Count; c++) {
+                    IForm += zSelectMathFunctions[c].Split('=')[0].Trim() + zEnter();
+                }
+                IForm += zEnter();
+                // Order By
+                if (zOrderBy.Count > 0) {
+                    IForm += "[ " + ++IFormStepsCounter + " : Order By ]" + zEnter();
+                    for (int c = 0; c < zOrderBy.Count; c++) {
+                        IForm += zOrderBy[c] + zEnter();
+                    }
+                    IForm += zEnter();
+                }
+                // [TextBox]
+                IForm = IForm.Substring(0, IForm.Length - 3);
+                txt_IF.Text = IForm;
+
+
+
+            } else {
+                MessageBox.Show(this, "This is not a ''Select Query''", "!!! ERROR !!!", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void btn_Q1_Click(object sender, EventArgs e) {
+            txt_IF.Text = txt_PF.Text = "";
+            txt_Query.Text = "Select" + zEnter() + "             S.SName" + zEnter() + "From" + zEnter() + "             S," + zEnter() + "             SP" + zEnter() + "Where" + zEnter() + "             S.S# = SP.S#" + zEnter() + "             And" + zEnter() + "             SP.P# = 'P2'" + zEnter() + "Group By" + zEnter() + "" + zEnter() + "Having" + zEnter() + "" + zEnter() + "Order By" + zEnter() + "";
+        }
+
+        private void btn_Q2_Click(object sender, EventArgs e) {
+            txt_IF.Text = txt_PF.Text = "";
+            txt_Query.Text = "Select" + zEnter() + "             S.SName" + zEnter() + "From" + zEnter() + "             S," + zEnter() + "             SP" + zEnter() + "Where" + zEnter() + "             S.S# = SP.S#" + zEnter() + "             And" + zEnter() + "             SP.P# = 'P2'" + zEnter() + "Group By" + zEnter() + "             S.SName" + zEnter() + "Having" + zEnter() + "             S.SName Like 'A%'" + zEnter() + "Order By" + zEnter() + "             S.SName Desc";
+        }
+
+        private void btn_Q3_Click(object sender, EventArgs e) {
+            txt_IF.Text = txt_PF.Text = "";
+            txt_Query.Text = "Select" + zEnter() + "             Product.Name," + zEnter() + "             Total = SUM(ProductInventory.Quantity)" + zEnter() + "From" + zEnter() + "             Product," + zEnter() + "             ProductInventory" + zEnter() + "Where" + zEnter() + "             Product.ProductId = ProductInventory.ProductId" + zEnter() + "             And" + zEnter() + "             Product.Name Like N'[A-G]%'" + zEnter() + "Group By" + zEnter() + "             Product.Name" + zEnter() + "Having" + zEnter() + "" + zEnter() + "Order By" + zEnter() + "";
+        }
+
+        private bool IsJoin(string Line) {
+            string[] Arr = Line.Split('=');
+            int Counter = 0;
+            for (int i = 0; i < Arr.Length; i++) {
+                string TableName = Arr[i].Split('.')[0].Trim();
+                for (int j = 0; j < zFrom.Count; j++) {
+                    if (TableName == zFrom[j]) {
+                        Counter++;
+                        break;
+                    }
+                }
+            }
+            return (Counter == 2 ? true : false);
+        }
+
+        private bool IsMath(string Line) {
+            return (Line.IndexOf("min(") != -1 || Line.IndexOf("max(") != -1 || Line.IndexOf("avg(") != -1 || Line.IndexOf("sum(") != -1 || Line.IndexOf("count(") != -1);
+        }
+
+        private string zEnter() {
+            return (char.ConvertFromUtf32(13) + char.ConvertFromUtf32(10));
+        }
+
+    }
+}
